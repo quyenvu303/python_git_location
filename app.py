528c9934e56ce14b178115e6e46053083ccd2c47
Đình Quyền <75819095+quyenvu303@users.noreply.github.com>
2025-07-18 15:57:36 +0700

    'add'

--- app.py
+++ app.py
@@ -1,24 +1,21 @@
 from flask import Flask, jsonify
 import requests
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-CORS(app)  # Kích hoạt CORS cho tất cả các route
+CORS(app)
 
 @app.route('/get-location', methods=['GET'])
 def get_location():
-    # API IP2Location
     api_url = 'https://api.ip2location.io/?key=8DBEB1CD87E525D0C3A64AABCC04D077&ip=116.99.6.135'
-
-    # Gửi yêu cầu GET tới API
     response = requests.get(api_url)
 
     if response.status_code == 200:
-        data = response.json()
-        return jsonify(data)  # Trả về dữ liệu JSON cho frontend
+        return jsonify(response.json())
     else:
         return jsonify({"error": "Không thể lấy dữ liệu từ API"}), 500
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 3000))  # Glitch yêu cầu PORT=3000
+    app.run(host='0.0.0.0', port=port)
